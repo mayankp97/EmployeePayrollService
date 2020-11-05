8d408067b38f85ff2c9d4a84fcf2a5e6f970d57e
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-05 12:54:15 +0530

    [Mayank] Add SQL Queries for UC 11, 12 and 13

--- EmployeeRepo.cs
+++ EmployeeRepo.cs
@@ -9,31 +9,52 @@ namespace EmployeePayrollService
     class EmployeeRepo
     {
         
-        public static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         static SqlConnection connection = new SqlConnection(connectionString);
-        public static DataSet GetAllEmployee()
+        public static void GetAllEmployee()
         {
-            try
-            {
-                var dataSet = new DataSet();
+            //try
+            //{
+                var employeeModel = new EmployeeModel();
                 using (connection)
                 {
+                    var query = @"Select * from employee_payroll";
+                    var sqlCommand = new SqlCommand(query, connection);
                     connection.Open();
-                    var adapter = new SqlDataAdapter("GetEmployeeList", connection);
-                    adapter.Fill(dataSet);
-                    Console.WriteLine("Done");
-                    connection.Close();
-                    return dataSet;
+                    var dataReader = sqlCommand.ExecuteReader();
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            employeeModel.EmployeeID = dataReader.GetInt32(0);
+                            employeeModel.EmployeeName = dataReader.GetString(1);
+                            employeeModel.Salary = dataReader.GetInt32(2);
+                            employeeModel.StartDate = dataReader.GetDateTime(3);
+                            employeeModel.Gender = Convert.ToChar(dataReader.GetString(4));
+                            employeeModel.PhoneNumber = dataReader.GetString(5);
+                            employeeModel.Address = dataReader.GetString(6);
+                            employeeModel.Department = dataReader.GetString(7);
+                            employeeModel.BasicPay = dataReader.GetInt32(8);
+                            employeeModel.TaxablePay = dataReader.GetInt32(9);
+                            employeeModel.Tax = dataReader.GetInt32(10);
+                            employeeModel.NetPay = dataReader.GetInt32(11);
+                            employeeModel.Display();
+
+                        }
+                    }
+                    else
+                        Console.WriteLine("No Data Found");
+                    
                 }
-            }
-            catch(Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                connection.Close();
-            }
+            //}
+            //catch(Exception ex)
+            //{
+            //    throw new Exception(ex.Message);
+            //}
+            //finally
+            //{
+            //    connection.Close();
+            //}
         }
     }
 }
---
+++ Queries - EmployeePayrollService/UC13_ImplementERDiagram.sql
@@ -0,0 +1,57 @@
+﻿use AddressBookDB;
+
+
+
+alter table ABookTable
+drop column PhoneNumber, Address, City, State, Email, ZipCode
+Go
+
+alter table ABookTable
+add primary key (Id)
+Go
+
+create table ContactInfo  
+(
+	Id Int Primary Key not null identity(1,1) foreign key references ABookTable(Id),
+	PhoneNumber varchar(13) not null,
+	Email varchar(25) not null
+)
+Go
+
+create table AddressInfo
+(
+	Id Int Primary Key not null identity(1,1) foreign key references ABookTable(Id),
+	Address varchar(15) not null,
+	City varchar(15) not null,
+	State varchar(20) not null,
+	Zipcode varchar(6) not null,
+)
+Go
+
+SET IDENTITY_INSERT dbo.AddressInfo ON
+Go
+
+INSERT INTO AddressInfo(Id,Address,City,State,Zipcode)
+     VALUES
+           (1,'28/A','Bhilai','C.G.','490006'),
+		   (3,'28/A','Hyd','T.L.','490566'),
+		   (4,'28/A','Vizag','A.P.','499876'),
+		   (5,'LtWllaims','Raigarh','C.G.','579893'),
+		   (6,'Ban/A','Bhellary','K.A.','780006')
+
+GO
+
+SET IDENTITY_INSERT dbo.AddressInfo OFF
+Go
+
+SET IDENTITY_INSERT dbo.ContactInfo ON
+Go
+
+INSERT INTO ContactInfo(Id,PhoneNumber,Email)
+     VALUES
+           (1,'9674879893','monee99@gmail.com'),
+		   (3,'8555466603','pru17@gmail.com'),
+		   (4,'7487009893','rajat002@gmail.com'),
+		   (5,'7487009893','ank675@gmail.com'),
+		   (6,'924579893','chin56@gmail.com')
+GO
