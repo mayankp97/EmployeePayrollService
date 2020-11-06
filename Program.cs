a70635f526085cd7e6ea2199ad23ece86fce6212
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-06 12:57:27 +0530

    [Mayank] Add function to retreive employees in a date range

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ namespace EmployeePayrollService
             Console.WriteLine("Welcome to Employee Payroll Service!");
 
             //EmployeeRepo.GetAllEmployee();
-            var salaryUpdateModel = new SalaryUpdateModel { SalaryId = 1, EmployeeSalary = 30000000 };
-            var salaryObj = new Salary();
-            var salary = salaryObj.UpdateEmployeeSalary(salaryUpdateModel);
-            Console.WriteLine(salary);
+            //var salaryUpdateModel = new SalaryUpdateModel { SalaryId = 1, EmployeeSalary = 30000000 };
+            //var salaryObj = new Salary();
+            //var salary = salaryObj.UpdateEmployeeSalary(salaryUpdateModel);
+            //Console.WriteLine(salary);
+            var startDate = Convert.ToDateTime("01/01/2018");
+            var endDate = Convert.ToDateTime("01/01/2019");
+
+            EmployeeRepo.GetAllEmployeeInDateRange(startDate, endDate);
         }
     }
 }
