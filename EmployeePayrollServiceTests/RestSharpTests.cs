53a1669fc93bc55e2007bed721aecf9ac8828575
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-18 10:27:56 +0530

    [Mayank] Add test for retrieving employees form json server using rest sharp

---
+++ EmployeePayrollServiceTests/RestSharpTests.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace EmployeePayrollServiceTests
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+    [TestFixture]
+    class RestSharpTests
+    {
+        RestClient client;
+        [SetUp]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:4000");
+        }
+        private IRestResponse GetAllEmployees()
+        {
+            var request = new RestRequest("/employees", Method.GET);
+
+            var response = client.Execute(request);
+
+            return response;
+        }
+
+        [Test]
+        public void GivenUrl_OnGet_ReturnsEmployees()
+        {
+            var response = GetAllEmployees();
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            var dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(dataResponse.Count, 4);
+            foreach (var employee in dataResponse)
+                Console.WriteLine("Id : {0}, Name : {1}, Salary : {2}", employee.Id, employee.Name, employee.Salary);
+
+
+        }
+    }
+}
---
+++ EmployeePayrollServiceTests/SalaryTest.cs
@@ -0,0 +1,30 @@
+﻿using EmployeePayrollService;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollServiceTests
+{
+    [TestFixture]
+    public class SalaryTest
+    {
+        [Test]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 2,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 5
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+}
